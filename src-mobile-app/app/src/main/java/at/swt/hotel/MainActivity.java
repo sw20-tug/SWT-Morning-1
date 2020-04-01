9e0eb6da90c4f1711b19fda4537e9a6428cbc668
Alexander Frewein <alexander.frewein@student.tugraz.at>
2020-04-01 09:44:11 +0200

    HOTEL_001A added view for scrolling hotels, added button event to switch to hotel, added first test classes [PW,AF]

--- src-mobile-app/app/src/main/java/at/swt/hotel/MainActivity.java
+++ src-mobile-app/app/src/main/java/at/swt/hotel/MainActivity.java
@@ -2,7 +2,10 @@ package at.swt.hotel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startTutorials = (Button) findViewById(R.id.btn_sort);
+        startTutorials.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tutorialsActivityIntent = new Intent(MainActivity.this, HotelViewActivity.class);
+                MainActivity.this.startActivity(tutorialsActivityIntent);
+
+            }
+        });
+
     }
 }
