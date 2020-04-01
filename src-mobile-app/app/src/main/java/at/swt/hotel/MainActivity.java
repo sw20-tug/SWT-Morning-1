b3aacfcc395d325a0fbe71cb3601dd70c762edc7
Alexander Frewein <alexander.frewein@student.tugraz.at>
2020-04-01 10:27:44 +0200

    HOTEL_016  create admin login [PW,AF]

--- src-mobile-app/app/src/main/java/at/swt/hotel/MainActivity.java
+++ src-mobile-app/app/src/main/java/at/swt/hotel/MainActivity.java
@@ -13,15 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button startTutorials = (Button) findViewById(R.id.btn_sort);
-        startTutorials.setOnClickListener(new View.OnClickListener() {
+        Button btn_sort = (Button) findViewById(R.id.btn_sort);
+        Button btn_login = (Button) findViewById(R.id.btnLogin);
+        switchToHotelView(btn_sort);
+        switchToLoginView(btn_login);
+    }
+
+
+    public void switchToHotelView(Button btn) {
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent tutorialsActivityIntent = new Intent(MainActivity.this, HotelViewActivity.class);
-                MainActivity.this.startActivity(tutorialsActivityIntent);
+                Intent HotelActivityIntent = new Intent(MainActivity.this, HotelViewActivity.class);
+                MainActivity.this.startActivity(HotelActivityIntent);
+
+            }
+         });
+    }
+
+    public void switchToLoginView(Button btn) {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LoginActivityIntent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(LoginActivityIntent);
 
             }
         });
-
     }
 }
